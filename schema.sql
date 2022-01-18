087aa7b94e7a265155022673ec569bc441478879
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-18 14:18:50 +0100

    Update schema: Remove trailing semi-colon

--- schema.sql
+++ schema.sql
@@ -9,5 +9,5 @@ CREATE TABLE IF NOT EXISTS animals
     date_of_birth DATE,
     escape_attempts INTEGER DEFAULT 0,
     neutered boolean DEFAULT false,
-    weight_kg NUMERIC,
-);
+    weight_kg NUMERIC
+)
