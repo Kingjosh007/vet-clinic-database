dcaa1909e89215fc59ad41a646184429b7d8601d
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-18 14:11:37 +0100

    Update schema: make id auto-increment

--- schema.sql
+++ schema.sql
@@ -1,19 +1,14 @@
--- Table: public.animals
+-- Table: animals
 
--- DROP TABLE IF EXISTS public.animals;
+DROP TABLE IF EXISTS animals;
 
-CREATE TABLE IF NOT EXISTS public.animals
+CREATE TABLE IF NOT EXISTS animals
 (
-    id integer NOT NULL,
-    name text COLLATE pg_catalog."default",
+    id integer NOT NULL AUTO_INCREMENT,
+    name text,
     date_of_birth date,
     escape_attempts integer DEFAULT 0,
     neutered boolean DEFAULT false,
     weight_kg numeric,
     CONSTRAINT animals_pkey PRIMARY KEY (id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.animals
-    OWNER to postgres;
+);
