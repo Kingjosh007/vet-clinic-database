a0f56d1917d6c4bd7d4035cdd2f319b74a3abd1d
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-18 13:36:32 +0100

    SQL code for creating animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,19 @@
-/* Database schema to keep the structure of entire database. */
+-- Table: public.animals
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+-- DROP TABLE IF EXISTS public.animals;
+
+CREATE TABLE IF NOT EXISTS public.animals
+(
+    id integer NOT NULL,
+    name text COLLATE pg_catalog."default",
+    date_of_birth date,
+    escape_attempts integer DEFAULT 0,
+    neutered boolean DEFAULT false,
+    weight_kg numeric,
+    CONSTRAINT animals_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.animals
+    OWNER to postgres;
