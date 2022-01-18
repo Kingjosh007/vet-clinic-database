1a9befba3ee4a0287ab46b0424c045a2df4eb3f4
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-18 14:17:07 +0100

    Update schema: capitalize all types

--- schema.sql
+++ schema.sql
@@ -4,11 +4,11 @@ DROP TABLE IF EXISTS animals;
 
 CREATE TABLE IF NOT EXISTS animals
 (
-    id integer NOT NULL AUTO_INCREMENT,
-    name text,
-    date_of_birth date,
-    escape_attempts integer DEFAULT 0,
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT,
+    date_of_birth DATE,
+    escape_attempts INTEGER DEFAULT 0,
     neutered boolean DEFAULT false,
-    weight_kg numeric,
+    weight_kg NUMERIC,
     CONSTRAINT animals_pkey PRIMARY KEY (id)
 );
