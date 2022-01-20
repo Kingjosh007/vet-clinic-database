ea270c703a149aef367b3bca03f6e55b8b52fe02
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-20 09:11:52 +0100

    Create owners table

--- schema.sql
+++ schema.sql
@@ -14,3 +14,12 @@ CREATE TABLE IF NOT EXISTS animals
 
 -- Add a column species of type string to your animals table
 ALTER TABLE animals ADD species TEXT;
+
+
+-- Create a table named owners
+DROP TABLE IF EXISTS owners;
+CREATE TABLE IF NOT EXISTS owners(
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ full_name TEXT,
+ age INT
+);
