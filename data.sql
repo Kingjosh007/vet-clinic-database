a7403cbd41e54679ae5242c09482e7ad8910abe7
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-20 09:25:42 +0100

    Populate owners table with data

--- data.sql
+++ data.sql
@@ -29,3 +29,13 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 
 -- Animal: Her name is Blossom. She was born on Oct 13th, 1998, and currently weighs 17kg. She is neutered and she has tried to escape 3 times.
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', '1998-10-13', 3, true, 17.0);
+
+
+-- Populate the owners table with data
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34),
+                                           ('Jennifer Orwell', 19),
+                                           ('Bob', 45),
+                                           ('Melody Pond', 77),
+                                           ('Dean Winchester', 14),
+                                           ('Jodie Whittaker', 38);
+
